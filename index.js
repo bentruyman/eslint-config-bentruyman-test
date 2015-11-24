555fbe558d3b2a81da3819d288196382a10d3973
Ben Truyman <ben@truyman.com>
2015-11-24 00:05:41 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "extends": "bentruyman",
+  "rules": {
+    "func-names": 0,
+    "max-nested-callbacks": 0,
+    "no-magic-numbers": 0,
+    "no-unused-expressions": 0
+  }
+};
